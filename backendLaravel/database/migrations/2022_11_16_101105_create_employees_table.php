da675249adf7a8a45392bf418db7d33d324cdf22
Muna Zeer <munaelzeer@gmail.com>
2022-11-20 08:14:50 -0800

    react-laravel

---
+++ backendLaravel/database/migrations/2022_11_16_101105_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('gender');
+            $table->string('phone');
+            $table->integer('salary');
+            $table->date('joinDate');
+            $table->date('BirthDate');
+            $table->string('employee-career');
+            $table->integer('DepID')->unsigned()->index()->nullable();
+            $table->foreign('DepID')->references('id')->on('departments')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
