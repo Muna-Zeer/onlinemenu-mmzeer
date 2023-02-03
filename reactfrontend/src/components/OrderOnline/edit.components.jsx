beb7e66d0164f575ff6dc28a49d0a78d729269f3
Muna Zeer <munaelzeer@gmail.com>
2023-02-03 09:49:59 +0200

    crudAPI_Order

--- reactfrontend/src/components/OrderOnline/edit.components.jsx
+++ reactfrontend/src/components/OrderOnline/edit.components.jsx
@@ -1,7 +1,10 @@
+import '../addOrder/addOrder.css';
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import swal from 'sweetalert';
+import { Handbag } from 'phosphor-react';
+
 
 const EditOrder = (props) => {
 
@@ -14,12 +17,12 @@ const EditOrder = (props) => {
     crust: '',
     Qty: '',
 
-});
+  });
   const [errorInput, setError] = useState([]);
   const [orders, setOrders] = useState([]);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("")
- 
+
   let { id } = useParams();
 
   useEffect(() => {
@@ -45,7 +48,7 @@ const EditOrder = (props) => {
     setOrder({ ...orderInput, [e.target.name]: e.target.value });
   }
 
-  const updateorder = async(e) => {
+  const updateorder = async (e) => {
     e.preventDefault();
 
     // const id = props.match.params.id;
@@ -56,10 +59,10 @@ const EditOrder = (props) => {
       crust: orderInput.crust
 
     }
-    
+
 
     await axios.put(` http://localhost:8000/api/update-order/${id}`, data).then(res => {
-      console.log('oo',res.json);
+      console.log('oo', res.json);
       if (res.data.status === 200) {
         swal("Success", res.data.message, "success");
         setError([]);
@@ -86,110 +89,77 @@ const EditOrder = (props) => {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6">
-            <div className="card">
+            <div className="card cardViewOrder m-5 ">
               <div className="card-header">
                 <h4>Edit orders
-                  <Link to={'/orders'} className="btn btn-danger btn-sm float-end"> BACK</Link>
+                  <Link to={'/ViewOrder'} className="btn btn-danger btn-sm float-end"> BACK</Link>
                 </h4>
               </div>
               <div className="card-body">
 
                 <form onSubmit={updateorder} >
-                  <ul>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="small"
-                          name='size'
-                          checked={orderInput.size === "small"}
-                          onChange={handleInput}
-                        />
-                        <span>small</span>
-                      </label>
-                    </li>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="large"
-                          name='size'
-                          checked={orderInput.size === "large"}
-                          onChange={handleInput}
-                        />
-                        <span>large</span>
-                      </label>
-                    </li>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="medium"
-                          name='size'
-                          checked={orderInput.size === "medium"}
-                          onChange={handleInput}
-                        />
-                        <span>medium</span>
-                      </label>
-                    </li>
+                  <div className='text-start'>
 
-                  </ul>
+                    <h5 >CHOOSE YOUR CRUST</h5>
+                    <ul className='text-muted'>
+                      <li>
+                        <label>
+                          <input type="radio" value="Classic Hand Tossed" name='crust'
+                            checked={orderInput.crust === "Classic Hand Tossed"} onChange={handleInput} />
+                          <span>Classic Hand Tossed</span>
+                        </label>
+                      </li>
+                      <li>
+                        <label>
+                          <input type="radio" value="Wheat Thin Crust" name='crust'
+                            checked={orderInput.crust === "Wheat Thin Crust"} onChange={handleInput} />
+                          <span>Wheat Thin Crust</span>
+                        </label>
+                      </li>
+                      <li>
+                        <label>
+                          <input type="radio" value="Classic Hand Tossed" name='crust'
+                            checked={orderInput.crust === "Classic Hand Tossed"} onChange={handleInput} />
+                          <span>Classic Hand Tossed</span>
+                        </label>
+                      </li>
 
+                    </ul>
+                    <h6 >SIZE OF CRUST</h6>
+                    <ul className='text-muted'>
+                      <li>
+                        <label>
+                          <input type="radio" value="small" name='size' checked={orderInput.size === "small"}
+                            onChange={handleInput} />
+                          <span>small</span>
+                        </label>
+                      </li>
+                      <li>
+                        <label>
+                          <input type="radio" value="large" name='size' checked={orderInput.size === "large"}
+                            onChange={handleInput} />
+                          <span>large</span>
+                        </label>
+                      </li>
+                      <li>
+                        <label>
+                          <input type="radio" value="medium" name='size' checked={orderInput.size === "medium"}
+                            onChange={handleInput} />
+                          <span>medium</span>
+                        </label>
+                      </li>
 
-
-
-                  <ul>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="Classic Hand Tossed"
-                          name='crust'
-                          checked={orderInput.crust === "Classic Hand Tossed"}
-                          onChange={handleInput}
-                        />
-                        <span>Classic Hand Tossed</span>
-                      </label>
-                    </li>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="Wheat Thin Crust"
-                          name='crust'
-                          checked={orderInput.crust === "Wheat Thin Crust"}
-                          onChange={handleInput}
-                        />
-                        <span>Wheat Thin Crust</span>
-                      </label>
-                    </li>
-                    <li>
-                      <label>
-                        <input
-                          type="radio"
-                          value="Classic Hand Tossed"
-                          name='crust'
-                          checked={orderInput.crust === "Classic Hand Tossed"}
-                          onChange={handleInput}
-                        />
-                        <span>Classic Hand Tossed</span>
-                      </label>
-                    </li>
-
-                  </ul>
-
-                  <div className="form-group mb-3">
-                    <label>order Qty</label>
-
-                    <input type="text" name="Qty" onChange={handleInput} value={orderInput.Qty} className="form-control" />
-                    <span className="text-danger">{errorInput.Qty}</span>
+                    </ul>
                   </div>
+                  <div className="form-Qty ">
+                    <label>Qty</label>
+                    <input type="number" placeholder='1' name="Qty" min='1' max='10' onChange={handleInput} value={orderInput.Qty} className="Qty" />
+                    <button type="submit" className="btn mb-3">
+                      {/* <Link className="nav-link" to="/ViewOrder"> */}
+                        <Handbag className='Handbag' size={32} color={'#000'} hover={'#000'} />Update
+                      {/* </Link> */}
+                    </button>
 
-
-
-
-                  <div className="form-group mb-3">
-                    <button type="submit" className="btn btn-primary">Save order</button>
                   </div>
                 </form>
 
